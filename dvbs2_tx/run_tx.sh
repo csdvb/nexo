efc71f10426ec1e15562366b4039649c1dfd6ff5
Alexandru Csete <oz9aec@gmail.com>
2017-08-13 17:52:26 +0200

    Update muxer and TX launcher

--- dvbs2_tx/run_tx.sh
+++ dvbs2_tx/run_tx.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
+#
+# Launch mptsd muxer and dvbs2_tx
+#
+
+PREFIX=/usr/local/etc
+MPTSD_CONF=$PREFIX/mptsd.conf
+MPTSD_CHAN=$PREFIX/mptsd_channels.conf
+MPTSD_EPG=$PREFIX/mptsd_epg.conf
+MPTSD_NIT=$PREFIX/mptsd_nit.conf
+
+/usr/local/bin/mptsd -O 127.0.0.1 -B 12.1 \
+    -g $MPTSD_CONF -c $MPTSD_CHAN -e $MPTSD_EPG -n $MPTSD_NIT &
 
 export VOLK_CONFIGPATH=/usr/local/etc
-
-/usr/local/bin/mptsd -O 127.0.0.1 -B 12.1 &
 /usr/local/bin/dvbs2_tx -u &
