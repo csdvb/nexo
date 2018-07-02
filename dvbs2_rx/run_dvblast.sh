05cdd53b342830b0884bb01cda8657f5c9630ac0
Alexandru Csete <oz9aec@gmail.com>
2018-07-03 00:00:12 +0200

    Ensure bias voltage is ON

--- dvbs2_rx/run_dvblast.sh
+++ dvbs2_rx/run_dvblast.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 
-#dvblast -r /tmp/dvblast -c streaming.conf -f 11030000 -s 6250000 -m psk_8 -a 0
-dvblast -r /tmp/dvblast -c lantest.conf -f 11030000 -s 6250000 -m psk_8 -a 0
+dvblast -r /tmp/dvblast -c streaming.conf -f 11030000 -s 6250000 -m psk_8 -a 0 -v 14
+#dvblast -r /tmp/dvblast -c lantest.conf -f 11030000 -s 6250000 -m psk_8 -a 0 -v 14
 
 # send full TS stream
 #dvblast -r /tmp/dvblast -f 11030000 -s 6250000 -m psk_8 -a 0 -d 192.168.10.50:14038 -U
